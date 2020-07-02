5dde4210947d75e8f9deb7c2b19f0622e32df464
harika penukonda <penukondaharika@gmail.com>
2020-07-02 11:14:08 -0500

    Create DependecyInjectionDemoApplication.java

---
+++ src/main/java/com/example/DependecyInjectionDemo/DependecyInjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DependecyInjectionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependecyInjectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependecyInjectionDemoApplication.class, args);
+	}
+
+}
