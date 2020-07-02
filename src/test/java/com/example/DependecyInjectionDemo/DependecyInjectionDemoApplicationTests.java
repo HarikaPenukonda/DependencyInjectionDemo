16f6349559d75dc60ea82f3422b58d27afcedc29
harika penukonda <penukondaharika@gmail.com>
2020-07-02 11:14:14 -0500

    Create DependecyInjectionDemoApplicationTests.java

---
+++ src/test/java/com/example/DependecyInjectionDemo/DependecyInjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DependecyInjectionDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependecyInjectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
