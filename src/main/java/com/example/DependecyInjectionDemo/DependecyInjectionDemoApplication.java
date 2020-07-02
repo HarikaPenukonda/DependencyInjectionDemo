cfa4cad0237693b498aa797575d371e9233a601d
harika penukonda <penukondaharika@gmail.com>
2020-07-02 12:36:30 -0500

    Update DependecyInjectionDemoApplication.java

--- src/main/java/com/example/DependecyInjectionDemo/DependecyInjectionDemoApplication.java
+++ src/main/java/com/example/DependecyInjectionDemo/DependecyInjectionDemoApplication.java
@@ -2,12 +2,17 @@ package com.example.DependecyInjectionDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependecyInjectionDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependecyInjectionDemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DependecyInjectionDemoApplication.class, args);
+		// ConfigurableApplicationContext returns an object at the time of execution
+		
+		Student s = context.getBean(Student.class);
+		s.display();
 	}
 
 }
